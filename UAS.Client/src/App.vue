9f04db9f93e8460f34ada61088f825cc0f554114
Marucik <rakoczy.mk@gmail.com>
2019-11-07 17:00:20 +0100

    C: Added Raleway as global font

--- UAS.Client/src/App.vue
+++ UAS.Client/src/App.vue
@@ -9,8 +9,14 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Raleway", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
