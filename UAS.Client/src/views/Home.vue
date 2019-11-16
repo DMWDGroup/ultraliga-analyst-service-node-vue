3a7239c17e91f87a766701ec35664b573f5a4bc4
Marucik <rakoczy.mk@gmail.com>
2019-11-16 22:02:31 +0100

    C: Added gradient as background

--- UAS.Client/src/views/Home.vue
+++ UAS.Client/src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
+    <TheHeader/>
   </div>
 </template>
 
 <script>
+import TheHeader from "../components/TheHeader.vue"
 
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    TheHeader
+  }
 };
 </script>
+
+<style lang="scss">
+.home {
+  min-height: 100vh;
+  background: $background;
+  background: linear-gradient(180deg, $background 0%, darken($background, 5%) 100%);
+}
+</style>
