cec0a043767d87adf949fa508547b7ffc870fffa
Marucik <rakoczy.mk@gmail.com>
2019-12-16 14:43:02 +0100

    C: Added number row component

--- UAS.Client/src/components/TheSeasonSummary.vue
+++ UAS.Client/src/components/TheSeasonSummary.vue
@@ -2,20 +2,22 @@
   <div class="season-summary">
     <SectionHeader title="podsumowanie" description="sezon <nazwa sezonu**>" />
     <TheSeasonSummaryTable />
+    <TheSeasonSummaryNumbers />
   </div>
 </template>
 
 <script>
 import SectionHeader from "@/components/SectionHeader.vue";
 import TheSeasonSummaryTable from "@/components/TheSeasonSummaryTable.vue";
+import TheSeasonSummaryNumbers from "@/components/TheSeasonSummaryNumbers.vue";
 
 export default {
   components: {
     SectionHeader,
-    TheSeasonSummaryTable
+    TheSeasonSummaryTable,
+    TheSeasonSummaryNumbers
   }
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
