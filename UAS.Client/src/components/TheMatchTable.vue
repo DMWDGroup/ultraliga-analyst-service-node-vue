3fbee6255e3c9f3a6e181c94291b9a6ae246b958
Marucik <rakoczy.mk@gmail.com>
2020-02-19 22:08:13 +0100

    C: Created scaffold to match table

---
+++ UAS.Client/src/components/TheMatchTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="match-table">
+    <h2 class="patch">
+      patch 9.12
+    </h2>
+    <div class="table-wrapper">
+      <div class="table-column table-border">
+        <TheMatchTableHeader :leftSide="true" />
+      </div>
+      <div class="table-column">
+        <TheMatchTableHeader :leftSide="false" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheMatchTableHeader from "@/components/TheMatchTableHeader.vue";
+
+export default {
+  components: { TheMatchTableHeader },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.patch {
+  font-size: 2rem;
+  font-weight: 100;
+  color: $text-primary;
+  text-align: center;
+  margin: 0.5rem;
+}
+
+.table-wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+}
+
+.table-border {
+  border-right: 1px solid $table-border;
+}
+</style>
---
+++ UAS.Client/src/components/TheMatchTableHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="table-header" :class="getSide">
+    <div class="gradient"></div>
+    <div class="kda"><img src="@/assets/img/skull.png" alt="skull icon" /></div>
+    <div class="items"><img src="@/assets/img/rucksack.png" alt="rucksack icon" /></div>
+    <div class="cs"><img src="@/assets/img/sword.png" alt="sword icon" /></div>
+    <div class="gold"><img src="@/assets/img/coins.png" alt="coins icon" /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    leftSide: Boolean
+  },
+  computed: {
+    getSide() {
+      if (this.leftSide) {
+        return "table-header__left";
+      } else {
+        return "table-header__right";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.table-header {
+  display: grid;
+  text-align: center;
+  border-top: 1px solid $table-border;
+  border-bottom: 1px solid $table-border;
+
+  img {
+    height: 2rem;
+  }
+
+  &__left {
+    grid-template-columns: [gradient] 38% [kda] 15% [items] 23% [cs] 11% [gold] auto;
+    grid-template-areas: "gradient kda items cs gold";
+
+    .gradient {
+      background: linear-gradient(to right, $red, transparent);
+      grid-area: gradient;
+    }
+  }
+
+  &__right {
+    grid-template-columns: [gold] auto [cs] 11% [items] 23% [kda] 15% [gradient] 38%;
+    grid-template-areas: "gold cs items kda gradient";
+
+    .gradient {
+      background: linear-gradient(to left, $blue, transparent);
+      grid-area: gradient;
+    }
+  }
+
+  .kda {
+    grid-area: kda;
+  }
+
+  .items {
+    grid-area: items;
+  }
+
+  .cs {
+    grid-area: cs;
+  }
+
+  .gold {
+    grid-area: gold;
+  }
+}
+</style>
